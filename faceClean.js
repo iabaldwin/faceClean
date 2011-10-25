f74e5dc67e3123d672572c0dcb842124bf1b7027
Ian Baldwin <ian@iabaldwin.com>
2011-10-25 13:55:23 +0100

    Reverted back to background page

--- faceClean.js
+++ faceClean.js
@@ -1,5 +1,4 @@
-function init() {
-
+function init( tab ) {
 
     if (!chrome.cookies) {
         chrome.cookies = chrome.experimental.cookies;
@@ -8,26 +7,20 @@ function init() {
 
     chrome.cookies.getAll( { "url":"http://www.facebook.com" }, 
             function( cookies ){ 
-            for ( var cookie_iterator=0; cookie_iterator < cookies.length; cookie_iterator++ ) {
-            chrome.cookies.remove( { "url":"http"+(cookies[cookie_iterator].secure ? "s" : "" )+ "://"+cookies[cookie_iterator].domain+cookies[cookie_iterator].path, "name":cookies[cookie_iterator].name}, 
-                function(details){ console.log( details.url ); } );
-            }
-
-            //if (cookies.length > 0)
-            //{
-            //var notification = webkitNotifications.createNotification(
-                //'icon.png',  
-                //'faceClean!',  // notification title
-                //'Removed '+cookies.length+' Facebook related ' + ( cookies.length == 1 ? 'cookie' : 'cookies'  )
-                //);
-
-            //notification.show();
-            //}
-            
-            }); 
-
-          
-            document.write( '<img src="icon.png" width="50"></img><p>Removed ' + cookies.length + ' cookies </p>' )
-            
-            });
-}
+                for ( var cookie_iterator=0; cookie_iterator < cookies.length; cookie_iterator++ ) {
+                chrome.cookies.remove( { "url":"http"+(cookies[cookie_iterator].secure ? "s" : "" )+ "://"+cookies[cookie_iterator].domain+cookies[cookie_iterator].path, "name":cookies[cookie_iterator].name}, 
+                    function(details){ console.log( details.url ); } );
+                }
+    
+    if ( cookies.length> 0 ){
+        chrome.browserAction.setBadgeBackgroundColor( { 'color':[255,0,0,255]})
+    }
+    else
+    {
+        chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,255,0,255]})
+    }
+    
+    chrome.browserAction.setBadgeText( { 'text': cookies.length.toString() } );
+    
+    }); 
+};
