e383e6fff6a69e9262465bd83eff4db42ca259cf
Ian Baldwin <ian@iabaldwin.com>
2011-10-25 14:55:00 +0100

    Changed color logic

--- faceClean.js
+++ faceClean.js
@@ -1,26 +1,46 @@
-function init( tab ) {
+function init() {
 
     if (!chrome.cookies) {
         chrome.cookies = chrome.experimental.cookies;
     }
 
+};
+
+
+function clean( tab ) {
 
     chrome.cookies.getAll( { "url":"http://www.facebook.com" }, 
             function( cookies ){ 
                 for ( var cookie_iterator=0; cookie_iterator < cookies.length; cookie_iterator++ ) {
-                chrome.cookies.remove( { "url":"http"+(cookies[cookie_iterator].secure ? "s" : "" )+ "://"+cookies[cookie_iterator].domain+cookies[cookie_iterator].path, "name":cookies[cookie_iterator].name}, 
-                    function(details){ console.log( details.url ); } );
+                    chrome.cookies.remove( { "url":"http"+(cookies[cookie_iterator].secure ? "s" : "" )+ "://"+cookies[cookie_iterator].domain+cookies[cookie_iterator].path, "name":cookies[cookie_iterator].name}, 
+                        function(details){ console.log( details.url ); } );
                 }
     
     if ( cookies.length> 0 ){
-        chrome.browserAction.setBadgeBackgroundColor( { 'color':[255,0,0,255]})
-    }
-    else
-    {
-        chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,255,0,255]})
+        chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,0,255,255]})
     }
     
     chrome.browserAction.setBadgeText( { 'text': cookies.length.toString() } );
     
     }); 
 };
+
+function monitor( tab ){ 
+
+    chrome.cookies.getAll( { "url":"http://www.facebook.com" }, 
+            function( cookies ){ 
+
+                if ( cookies.length> 0 ){
+                    chrome.browserAction.setBadgeBackgroundColor( { 'color':[255,0,0,255]})
+                }
+                else
+                {
+                    chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,255,0,255]})
+                }
+
+                chrome.browserAction.setBadgeText( { 'text': cookies.length.toString() } );
+
+                });
+
+};
+
