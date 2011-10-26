e05d978ca7f7db385da67f31cd54c19e4af0f185
Ian Baldwin <ian@iabaldwin.com>
2011-10-26 16:44:04 +0100

    More intuitive use

--- faceClean.js
+++ faceClean.js
@@ -17,21 +17,22 @@ function clean( tab ) {
 
     chrome.cookies.getAll( { "url":"http://www.facebook.com" }, 
             function( cookies ){ 
+                
+                if ( cookies.length> 0 ){
+                    chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,0,255,255]})
+                }
+                
+                chrome.browserAction.setBadgeText( { 'text': cookies.length.toString() } );
+    
                 for ( var cookie_iterator=0; cookie_iterator < cookies.length; cookie_iterator++ ) {
                     chrome.cookies.remove( { "url":"http"+(cookies[cookie_iterator].secure ? "s" : "" )+ "://"+cookies[cookie_iterator].domain+cookies[cookie_iterator].path, "name":cookies[cookie_iterator].name} );
                 }
     
-    if ( cookies.length> 0 ){
-        chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,0,255,255]})
-    }
-    
-    chrome.browserAction.setBadgeText( { 'text': cookies.length.toString() } );
-
-    setTimeout( function(){ 
-        chrome.browserAction.setBadgeText( { 'text':String(0)  } );
-        chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,255,0,255]})
-        }
-        ,1000 );
+                setTimeout( function(){ 
+                chrome.browserAction.setBadgeText( { 'text':String(0)  } );
+                chrome.browserAction.setBadgeBackgroundColor( { 'color':[0,255,0,255]})
+                }
+                ,1000 );
 
     }); 
 };
